0724c6f2112897c8bc7ea19f8f951e1111cd0778
akio suzuki <akio.suzuki@toranne.net>
2015-03-09 23:30:12 +0900

    fc21 1st-new

---
+++ mkdot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s .dot/bashrc .bashrc
+ln -s .dot/git-completion.bash .git-completion.bash
+ln -s .dot/git-flow-completion.bash .git-flow-completion.bash
+ln -s .dot/git-prompt.sh .git-prompt.sh
+ln -s .dot/gitconfig .gitconfig
+ln -s .dot/gitignore .gitignore
+ln -s .dot/screenrc .screenrc
+ln -s .dot/vim .vim
+ln -s .dot/vimrc .vimrc
